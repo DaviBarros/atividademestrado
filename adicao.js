3e4db3c36ee23d4b7a9527e2f6a3f2d1780bd06b
davibarros <davibarros@hotmail.com>
2025-06-20 17:46:51 -0300

    alteração da mensagem de adição

--- adicao.js
+++ adicao.js
@@ -1,6 +1,6 @@
 function somar(a, b) {
     const resultado = a + b;
-    console.log(`O resultado da soma entre ${a} e ${b} é ${resultado}.`);
+    console.log(`A soma entre ${a} e ${b} é ${resultado}.`);
 }
 
 
